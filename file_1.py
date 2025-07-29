2d3b2f80e7d98ea1599eeb29daf50b20c385e6d1
Cheikh Baye <cheikh.baye.boye-ext@adias.fr>
2025-07-29 12:41:52 +0000

    to be merged to master branch

--- file_1.py
+++ file_1.py
@@ -2,3 +2,11 @@ print("hello world")
 
 for i in range(10):
     print(i)
+
+import os
+import pandas as pd
+import numpy as np
+
+print("This is a test file for git tutorial.")
+print("This file is used to demonstrate git commands.")
+print("This is a new line added to the file.")
